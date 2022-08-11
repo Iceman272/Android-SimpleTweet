585775e68f0e47187bd78308ba69ad37c670ad66
Iceman272 <72137901+Iceman272@users.noreply.github.com>
2022-08-11 18:42:26 -0400

    Finished Project

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -1,8 +1,12 @@
 package com.codepath.apps.restclienttemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -55,6 +59,33 @@ class TimelineActivity : AppCompatActivity() {
         populateHomeTimeline()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.compose){
+            val intent = Intent(this, ComposeActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if(resultCode == RESULT_OK && resultCode == REQUEST_CODE){
+            val tweet = data?.getParcelableExtra("tweet") as Tweet
+
+            tweets.add(0, tweet)
+
+            adapter.notifyItemInserted(0)
+            rvTweets.smoothScrollToPosition(0)
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
 
     fun populateHomeTimeline(){
         client.getHomeTimeline(object: JsonHttpResponseHandler(){
@@ -67,6 +98,7 @@ class TimelineActivity : AppCompatActivity() {
                 try {
                     adapter.clear()
                     val listOfNewTweetsReceived = Tweet.fromJsonArray(jsonArray)
+                    //possibly make a for (list.length) loop with a if (search.isEmpty() || Search == user.name) maybe check is empty and substring instead = tweets.add(list[x]) else normal code
                     tweets.addAll(listOfNewTweetsReceived)
                     adapter.notifyDataSetChanged()
                     // Now we call setRefreshing(false) to signal refresh has finished
@@ -91,6 +123,7 @@ class TimelineActivity : AppCompatActivity() {
     }
 
     companion object{
-         val TAG = "TimelineActivity"
+        val TAG = "TimelineActivity"
+        val REQUEST_CODE = 10
     }
 }
